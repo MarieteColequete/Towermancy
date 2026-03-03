d0528c39a337d36bcd84435e6aea2e2fe6edb0b5
Mario Cuéllar <34687013+MarieteColequete@users.noreply.github.com>
2026-03-03 13:47:58 +0100

    Refactor chunk path connections and drawing
    Replace string-based connection detection with an enum-backed Direction and internal _connections dictionary. Add APIs: get_connections, get_active_connections, is_connection_active, draw_connections and _draw_path_from_center to paint path tiles from the chunk center toward specified directions. The previous tilemap-scanning logic and direction computation are left commented out as legacy code. Also update chunk.tscn's tile_map_data to match the revised tile layout.

--- map/chunks/chunk.gd
+++ map/chunks/chunk.gd
@@ -4,62 +4,64 @@ extends Node2D
 @onready var world_tilemap: TileMapLayer = $WorldTiles
 @onready var path_tilemap: TileMapLayer = $PathTiles
 
-#var directions := {
-	#0: "dead_end",
-	#1: "straight",
-	#2: "turn",
-	#3: "t_cross",
-	#4: "x_cross"
-#}
+enum Direction { NORTH, SOUTH , EAST, WEST}
+
+var _connections := {
+	Direction.NORTH: false,
+	Direction.SOUTH: false,
+	Direction.EAST: false,
+	Direction.WEST: false
+}
 
 func _ready():
 	assert(chunk_size.x % 2 != 0, "Chunk size must be odd")
 	assert(chunk_size.y % 2 != 0, "Chunk size must be odd")
 	pass
 
-func get_connections() -> Dictionary :
-	var middle: Vector2i = chunk_size / 2
-	var output := {
-	"north": false,
-	"south": false,
-	"east": false,
-	"west": false
-	}
-	
-	if path_tilemap.get_cell_tile_data(Vector2i(middle.x, 0)) != null:
-		output.north = true
-	if path_tilemap.get_cell_tile_data(Vector2i(middle.x, chunk_size.y - 1)) != null:
-		output.south = true
-	if path_tilemap.get_cell_tile_data(Vector2i(0, middle.y)) != null:
-		output.west = true
-	if path_tilemap.get_cell_tile_data(Vector2i(chunk_size.x - 1, middle.y)) != null:
-		output.east = true
-	
-	return output
+func get_connections() -> Dictionary[StringName, bool]:
+	return _connections;	
 
-func _get_path_amount(connections: Dictionary) -> int:
-	var amount: int = 0
-	for x in connections:
-		if connections[x] == true: amount += 1
-	
-	assert(amount > 0, "There are no paths")
-	return amount
+func get_active_connections() -> Dictionary[StringName, bool]:
+	var result: Dictionary[StringName, bool]
+	for i in _connections:
+		if i == true: result.assign(i)
+	return result
 
-func get_direction() -> String:
-	var connections := get_connections()
-	var path_amount: int = _get_path_amount(connections)
+func is_connection_active(dir: Direction) -> bool:
+	return _connections.get(dir, false)
+
+func clear_connections():
+	for dir in _connections:
+		_connections[dir] = false
+
+func draw_connections(directions: Array[Direction]) -> void:
+	clear_connections()
 	
-	match path_amount:
-		1:
-			return "dead_end"
-		2:
-			if (connections.get("north") == true and connections.get("south") == true) or (connections.get("west") == true and connections.get("east") == true):
-				return "straight"
-			else: return "turn"
-		3:
-			return "t_cross"
-		4:
-			return "x_cross"
-	
-	assert(true, "No direction found")
-	return "not found"
+	for dir in directions:
+		_connections[dir] = true
+		_draw_path_from_center(dir)
+
+func _draw_path_from_center(dir: Direction) -> void:
+	var middle := Vector2i(chunk_size.x / 2, chunk_size.y / 2)
+	var pos := middle
+
+	match dir:
+		Direction.NORTH:
+			while pos.y > 0:
+				path_tilemap.set_cell(pos, 0)
+				pos.y -= 1
+
+		Direction.SOUTH:
+			while pos.y < chunk_size.y - 1:
+				path_tilemap.set_cell(pos, 0)
+				pos.y += 1
+
+		Direction.WEST:
+			while pos.x > 0:
+				path_tilemap.set_cell(pos, 0)
+				pos.x -= 1
+
+		Direction.EAST:
+			while pos.x < chunk_size.x - 1:
+				path_tilemap.set_cell(pos, 0)
+				pos.x += 1
